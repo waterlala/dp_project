4944b1c3144c70648cd500b13c220fa8afe9ba99
rmp871103 <47851411+rmp871103@users.noreply.github.com>
2020-06-03 08:56:19 +0800

    cycle test1

--- app/code/DataStorage.py
+++ app/code/DataStorage.py
@@ -3,8 +3,16 @@ from Patient import Patient
 class DataStorage():
     def __init__(self):
         self._patient_list = []
+        self._calculated_result = {}
 
     def add_patient(self, patinet):
         self._patient_list.append(patinet)
-    def accept_visitor(self, visitor):
-        visitor.visit_storage(self)
+
+    def add_calculated_result(self, result_name, result):
+        self._calculated_result[result_name] = result
+
+    def iter_patient_list(self):
+        return self._patient_list
+
+    def calculate_result(self, select_result):
+        return self._calculated_result[select_result]
