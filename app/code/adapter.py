e2d22cf6d3e0b341b5e388434c20b38f2ca1404d
rmp871103 <47851411+rmp871103@users.noreply.github.com>
2020-06-05 08:38:03 +0800

    adapter test1

--- app/code/adapter.py
+++ app/code/adapter.py
@@ -1,10 +1,20 @@
+import pandas as pd
+import numpy as np
+import json
 from InOutput import InOutput
 
 #需全改 IO 要回傳dataframe 再用adapter 轉 json回傳
-class Adapter:
-    def __init__(self):
-        pass
-    def dataframe_to_json(self, dataframe):
-        pass
-        # return self.result
-        
+class Adapter(InOutput):
+    def __init__(self, io):
+        self.__io = io
+    def handle_instructions(self, instructions, id):
+        result = self.__io.handle_instructions(instructions, id)
+        if(isinstance(result, None)):
+            return "{}"
+        elif(isinstance(result, pd.DataFrame)):
+            return result.to_json(orient='index')
+        elif(isinstance(result,float)):
+            float_dict = {}
+            float_dict["instructions"] = result
+            return json.dumps(float_dict)
+            
--- app/code/api.py
+++ app/code/api.py
@@ -6,40 +6,37 @@ from InOutput import InOutput
 app = Flask(__name__)
 
 io = InOutput()
-
-@app.route('/set_patient', methods=['POST'])
-def set_patient():
-    patientid = request.values.get('patientid')
-    io.set_patient_by_id(patientid)
-    return str(patientid)
-
+df_json_adapter = Adapter(io)
 
 @app.route('/personal_data', methods=['POST'])
 def personal_data():
-    return str(io.get_patient_personal_data())
-
+    patientid = request.values.get('patientid')
+    return str(df_json_adapter.handle_instructions("get_personal_info",patientid))
 
 @app.route('/icd_card', methods=['POST'])
 def icd_card():
-    return str(io.get_patients_icd_card())
-
-
-@app.route('/pattern_patients', methods=['POST'])
-def pattern_patients():
-    return str(io.get_patients_by_pattern())
-
-
-@app.route('/shorterm_death_prob', methods=['POST'])
-def shorterm_death_prob():
-    return str(io.get_quickly_death_model_point())
-
-
-@app.route('/inpatient_mean', methods=['POST'])
-def inpatient_mean():
-    return str(cycle.get_average_of_inpatient_cycle_of_everypatient())
-
+    patientid = request.values.get('patientid')
+    return str(df_json_adapter.handle_instructions("get_icd_category_growth_trend",patientid))
 
 @app.route('/inpatient', methods=['POST'])
 def inpatient():
     patientid = request.values.get('patientid')
-    return str(cycle.get_inpatient_cycle_of_single_patient(patientid))
+    return str(df_json_adapter.handle_instructions("get_personal_inpatient_cycle",patientid))
+
+@app.route('/inpatient_mean', methods=['POST'])
+def inpatient_mean():
+    return str(df_json_adapter.handle_instructions("get_average_of_all_the_patients_cycles",""))
+
+@app.route('/shorterm_death_prob', methods=['POST'])
+def shorterm_death_prob():
+    patientid = request.values.get('patientid')
+    return str(df_json_adapter.handle_instructions("get_shortterm_mortality_rate",patientid))
+
+@app.route('/pattern_patients', methods=['POST'])
+def pattern_patients():
+    patientid = request.values.get('patientid')
+    return str(df_json_adapter.handle_instructions(,patientid))
+
+
+
+
